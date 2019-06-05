42f74befd95c128dece13a225215d34a723b0872
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2019-06-05 16:58:36 -0400

    Re-exporting emf.ecore dependency.

---
+++ emf/src/org/nasdanika/emf/Protected.java
@@ -0,0 +1,12 @@
+package org.nasdanika.emf;
+
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl.Container;
+
+/**
+ * Base class for EObject's with built-in access control by adapting to {@link AccessController}.
+ * @author Pavel
+ *
+ */
+public class Protected extends Container {
+
+}
