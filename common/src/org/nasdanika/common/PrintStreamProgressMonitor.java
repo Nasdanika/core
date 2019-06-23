54720918d604b071e92f780695c0aa368e021112
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2019-06-23 12:01:12 -0400

    PrintStreamProgressMonitor.

---
+++ common/src/org/nasdanika/common/PrintStreamProgressMonitor.java
@@ -0,0 +1,89 @@
+package org.nasdanika.common;
+
+import java.io.PrintStream;
+import java.util.Arrays;
+
+/**
+ * Progress monitor reporting to a {@link PrintStream}, e.g. ``System.out``.
+ * This monitor indents sub-tasks and worked messages. It can be thought of as a hierarchical logger.
+ * @author Pavel
+ *
+ */
+public class PrintStreamProgressMonitor implements ProgressMonitor {
+
+	private PrintStream out;
+	private boolean closeStream;
+	private String indent;
+	private int indentIncrement;
+	private boolean cancelled;
+	
+	/**
+	 * Constructs a progress monitor for a given print stream.
+	 * @param out
+	 * @param indent Indent in spaces for this monitor.
+	 * @param indentIncrement Increment to add to the indent of this monitor when constructing a sub-monitor.
+	 * @param closeStream if true the monitor closes the stream in its close() method.
+	 */
+	public PrintStreamProgressMonitor(PrintStream out, int indent, int indentIncrement, boolean closeStream) {
+		this.out = out;
+		StringBuilder indentBuilder = new StringBuilder();
+		for (int i = 0; i < indent; ++i) {
+			indentBuilder.append(' ');
+		}
+		this.indent = indentBuilder.toString();
+		this.closeStream = closeStream;
+	}
+	
+	/**
+	 * Constructs a progres monitor outputting to System.out with indent 0, indentIncrement 2 and not closing the stream.
+	 */
+	public PrintStreamProgressMonitor() {
+		this(System.out, 0, 2, false);
+	}
+	
+	@Override
+	public void close() {
+		if (closeStream) {
+			out.close();
+		}
+	}
+
+	@Override
+	public void cancel() {
+		cancelled = true;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return cancelled;
+	}
+
+	@Override
+	public ProgressMonitor split(String taskName, int ticks, Object... details) {
+		out.println(indent+"  "+taskName+"("+ticks+") "+Arrays.toString(details));
+		return new PrintStreamProgressMonitor(out, indent.length()+indentIncrement, indentIncrement, false) {
+			
+			@Override
+			public boolean isCancelled() {
+				return PrintStreamProgressMonitor.this.isCancelled();
+			}
+			
+			@Override
+			public void cancel() {
+				PrintStreamProgressMonitor.this.cancel();
+			}
+			
+		};
+	}
+
+	@Override
+	public void worked(int work, String progressMessage) {
+		out.println(indent+"  ["+work+"] "+progressMessage);
+	}
+
+	@Override
+	public void setWorkRemained(int ticks) {
+		// NOP
+	}
+
+}
