8cff781ce4f21de40a3674e51ed0d98c11aab99d
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2019-07-30 19:28:57 -0400

    Fix.

--- emf.edit/src/org/nasdanika/emf/edit/NasdanikaItemProviderAdapter.java
+++ emf.edit/src/org/nasdanika/emf/edit/NasdanikaItemProviderAdapter.java
@@ -1,12 +1,17 @@
 package org.nasdanika.emf.edit;
 
+import java.util.Collection;
 import java.util.Locale;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.util.ResourceLocator;
 import org.eclipse.emf.ecore.EModelElement;
 import org.eclipse.emf.ecore.ENamedElement;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 import org.jsoup.Jsoup;
 import org.nasdanika.common.Context;
@@ -67,6 +72,91 @@ public class NasdanikaItemProviderAdapter extends ItemProviderAdapter {
 		return new SingleReferenceDialogCellEditorFactory(null, ((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory());
 	}
 	
+	@Override
+  protected ItemPropertyDescriptor createItemPropertyDescriptor(
+		    AdapterFactory adapterFactory,
+		    ResourceLocator resourceLocator,
+		    String displayName,
+		    String description,
+		    EStructuralFeature feature,
+		    boolean isSettable,
+		    boolean multiLine,
+		    boolean sortChoices,
+		    Object staticImage,
+		    String category,
+		    String[] filterFlags)
+		  {			
+		    Object cellEditorFactory =  null;
+		    if (feature instanceof EReference) {
+		    	cellEditorFactory = feature.isMany() ? createMultiReferenceDialogCellEditorFactory() : createSingleReferenceDialogCellEditorFactory();
+		    }
+			return createItemPropertyDescriptor
+		      (adapterFactory,
+		       resourceLocator,
+		       displayName,
+		       description,
+		       feature,
+		       isSettable,
+		       multiLine,
+		       sortChoices,
+		       staticImage,
+		       category,
+		       filterFlags,
+		       cellEditorFactory);
+		  }
 
+	/**
+	 * Creates an item property descriptor with description loaded from a model annotation, display name also loaded from a model annotation for localization.
+	 * Creates a cell editor factory for {@link EReference} based on isMany().
+	 * @param resourceLocator
+	 * @param displayName
+	 * @param feature
+	 * @param isSettable
+	 * @param multiLine
+	 * @param sortChoices
+	 * @param staticImage
+	 * @param category
+	 * @param filterFlags
+	 * @param choiceOfValues Choice of values, the inherited choice of values is used if this one is null.
+	 * @return
+	 */
+	  protected ItemPropertyDescriptor createItemPropertyDescriptor(
+			    ResourceLocator resourceLocator,
+			    String displayName,
+			    EStructuralFeature feature,
+			    boolean isSettable,
+			    boolean multiLine,
+			    boolean sortChoices,
+			    Object staticImage,
+			    String category,
+			    String[] filterFlags, 
+			    Collection<Object> choiceOfValues)
+			  {			
+			    Object cellEditorFactory =  null;
+			    if (feature instanceof EReference) {
+			    	cellEditorFactory = feature.isMany() ? createMultiReferenceDialogCellEditorFactory() : createSingleReferenceDialogCellEditorFactory();
+			    }
+				return 
+				  new ItemPropertyDescriptor
+				    (((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
+				     resourceLocator,
+				     getLabel(feature, displayName),
+				     getTooltip(feature),
+				     feature,
+				     isSettable,
+				     multiLine,
+				     sortChoices,
+				     staticImage,
+				     category,
+				     filterFlags,
+				     cellEditorFactory) {
+					
+					@Override
+					protected Collection<?> getComboBoxObjects(Object object) {
+						return choiceOfValues == null ? super.getComboBoxObjects(object) : choiceOfValues;
+					}
+					
+				};
+			  }
 	
 }
