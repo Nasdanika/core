f680126790e1b7ea1baf5efa7fea974b433e1fb2
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2019-09-07 14:14:13 -0400

    Ability to attach ProgressRecorder.

--- common/src/org/nasdanika/common/NasdanikaException.java
+++ common/src/org/nasdanika/common/NasdanikaException.java
@@ -23,5 +23,45 @@ public class NasdanikaException extends RuntimeException {
 	public NasdanikaException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
 	}
+	
+	protected ProgressRecorder progressRecorder;
+	
+	/**
+	 * Creates an exception with recorder progress which can be used for troubleshooting.
+	 * @param message
+	 * @param progressRecorder
+	 */
+	public NasdanikaException(String message, ProgressRecorder progressRecorder) {
+		this(message);
+		this.progressRecorder = progressRecorder;
+	}
+
+	/**
+	 * Creates an exception with recorder progress which can be used for troubleshooting.
+	 * @param cause
+	 * @param progressRecorder
+	 */
+	public NasdanikaException(Throwable cause, ProgressRecorder progressRecorder) {
+		this(cause);
+		this.progressRecorder = progressRecorder;
+	}
+
+	/**
+	 * Creates an exception with recorder progress which can be used for troubleshooting.
+	 * @param message
+	 * @param cause
+	 * @param progressRecorder
+	 */
+	public NasdanikaException(String message, Throwable cause, ProgressRecorder progressRecorder) {
+		this(message, cause);
+		this.progressRecorder = progressRecorder;
+	}
+
+	/**
+	 * @return progress recorder which can be used for troubleshooting.
+	 */
+	public ProgressRecorder getProgressRecorder() {
+		return progressRecorder;
+	}
 
 }
