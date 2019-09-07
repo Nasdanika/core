168c41a60087be1d23bb8501c29da3aaee972323
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2019-09-07 19:36:29 -0400

    Refinements.

--- common/src/org/nasdanika/common/JavaExpressionPropertyComputer.java
+++ common/src/org/nasdanika/common/JavaExpressionPropertyComputer.java
@@ -7,12 +7,22 @@ import org.codehaus.commons.compiler.CompileException;
 import org.codehaus.janino.ExpressionEvaluator;
 
 /**
- * Evalates java expressions passed as sub-keys for this property key. E.g. <code>2+2</code> in <code>eval/2+2</code> if the property key is eval.
+ * Evaluates java expressions passed as sub-keys for this property key. E.g. <code>2+2</code> in <code>eval/2+2</code> if the property key is eval.
  * @author Pavel
  *
  */
 public class JavaExpressionPropertyComputer implements PropertyComputer {
+	
+	private ClassLoader classLoader;
 
+
+	public JavaExpressionPropertyComputer() {
+		this(JavaExpressionPropertyComputer.class.getClassLoader());
+	}
+	
+	public JavaExpressionPropertyComputer(ClassLoader classLoader) {
+		this.classLoader = classLoader;
+	}
 
 	@SuppressWarnings("unchecked")
 	@Override
@@ -22,6 +32,7 @@ public class JavaExpressionPropertyComputer implements PropertyComputer {
 			@Override
 			public Object apply(String expression) {
 				ExpressionEvaluator expressionEvaluator = new ExpressionEvaluator();
+				expressionEvaluator.setParentClassLoader(classLoader);
 				expressionEvaluator.setExpressionType(type);
 				expressionEvaluator.setParameters(new String[] { "context" }, new Class[] { Context.class });
 				try {
