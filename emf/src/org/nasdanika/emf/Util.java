b48f737174d6171626047268e37067c8851225a7
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2019-11-30 13:03:52 -0500

    Ongoing improvements.

--- emf/src/org/nasdanika/emf/Util.java
+++ emf/src/org/nasdanika/emf/Util.java
@@ -1,7 +1,13 @@
 package org.nasdanika.emf;
 
+import java.net.URL;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EAnnotation;
 import org.eclipse.emf.ecore.EModelElement;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.osgi.framework.Bundle;
 
 public class Util {
 
@@ -22,6 +28,34 @@ public class Util {
 		EAnnotation na = getNasdanikaAnnotation(modelElement);
 		return na == null ? null : na.getDetails().get("category");		
 	}
+	
+	public static final String BUNDLE_PROTOCOL = "bundle://";
+	
+	/**
+	 * Resolves reference relative to the resource. <code>bundle://</code> protocol can be used to reference resources in OSGi bundles similarly to <code>plugin://</code> protocol.  
+	 * @param resource Used to resolve relative references if not null.
+	 * @param reference Reference.
+	 * @return
+	 * @throws Exception
+	 */
+	public static URL resolveReference(Resource resource, String reference) throws Exception {
+		if (reference.startsWith(BUNDLE_PROTOCOL)) {
+			String bp = reference.substring(BUNDLE_PROTOCOL.length());
+			int slashIdx = bp.indexOf("/");
+			Bundle bundle = Platform.getBundle(bp.substring(0, slashIdx));
+			return bundle.getEntry(bp.substring(slashIdx));
+		}
+		
+		if (resource != null) {
+			URI resUri = resource.getURI();
+			URI refUri = URI.createURI(reference);
+			URI resolvedUri = refUri.resolve(resUri);
+			return new URL(resolvedUri.toString());			
+		}
+		
+		return new URL(reference);
+	}
+	
 	
 
 }
