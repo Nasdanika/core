f4d25f824f7dc1ff148699188417e47eda4f4d83
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2020-04-06 01:58:14 -0400

    Commented out root command icon.

--- cli/src/org/nasdanika/cli/Application.java
+++ cli/src/org/nasdanika/cli/Application.java
@@ -28,7 +28,7 @@ public class Application implements IApplication {
 			name="nsd", 
 			mixinStandardHelpOptions = true, 
 			versionProvider = BundleVersionProvider.class)
-	@Description(icon = "las la-terminal")
+//	@Description(icon = "las la-terminal")
 	public static class RootCommand extends CommandGroup {
 		
 	}
