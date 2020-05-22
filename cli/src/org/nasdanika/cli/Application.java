5713ae3ac00d37ef25ede3a182baa0bed767c48f
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2020-05-21 22:46:35 -0400

    Sorting sub-commands by name.

--- cli/src/org/nasdanika/cli/Application.java
+++ cli/src/org/nasdanika/cli/Application.java
@@ -28,6 +28,7 @@ public class Application implements IApplication {
 			name="nsd", 
 			mixinStandardHelpOptions = true, 
 			versionProvider = BundleVersionProvider.class)
+	
 //	@Description(icon = "las la-terminal")
 	public static class RootCommand extends CommandGroup {
 		
@@ -91,6 +92,7 @@ public class Application implements IApplication {
 				}
 			}
 		}
+		ret.sort((a,b) -> a.getCommandName().compareTo(b.getCommandName()));
 		return ret;
 	}
 	
