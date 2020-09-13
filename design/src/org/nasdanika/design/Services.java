15f996b19230c15fb75736fd2600d4495da72c34
Pavel <Pavel.Vlasov@nasdanika.org>
2020-09-13 13:00:30 -0400

    Initial design - copied from codegen and modified some files.

---
+++ design/src/org/nasdanika/design/Services.java
@@ -0,0 +1,10 @@
+package org.nasdanika.codegen.design;
+
+import org.nasdanika.sirius.CommonServices;
+
+/**
+ * The services class used by VSM.
+ */
+public class Services extends CommonServices {
+
+}
