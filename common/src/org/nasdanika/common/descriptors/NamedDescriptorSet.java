7eae8311019db1f12028187c0c7eff2f6797671d
Pavel <Pavel.Vlasov@nasdanika.org>
2021-01-03 16:11:50 -0500

    NamedDescriptorSet for convenience.

---
+++ common/src/org/nasdanika/common/descriptors/NamedDescriptorSet.java
@@ -0,0 +1,10 @@
+package org.nasdanika.common.descriptors;
+
+/**
+ * Descriptor of something with a name.
+ * @author Pavel
+ *
+ */
+public interface NamedDescriptorSet extends DescriptorSet, NamedDescriptor {
+
+}
