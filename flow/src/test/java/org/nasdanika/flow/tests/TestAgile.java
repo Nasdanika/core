bf58f54eb86dd17ae326a357d33501923098bdfc
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2021-09-30 19:47:28 +0100

    Inheritance tests.

--- flow/src/test/java/org/nasdanika/flow/tests/TestAgile.java
+++ flow/src/test/java/org/nasdanika/flow/tests/TestAgile.java
@@ -100,4 +100,130 @@ public class TestAgile extends TestBase {
 		
 	}
 	
+	@Test
+	public void testJava() throws Exception {	
+		load(
+				"agile/java.yml", 
+				obj -> {
+					org.nasdanika.flow.Package java = (org.nasdanika.flow.Package) obj;					
+					Package javaInstance = java.create();
+					ResourceSet resourceSet = new NcoreResourceSet();
+					resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(org.eclipse.emf.ecore.resource.Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
+					org.eclipse.emf.ecore.resource.Resource resource = resourceSet.createResource(URI.createURI("mem://xxx/java-instance.xml"));
+					resource.getContents().add(javaInstance);
+//					try {
+//						resource.save(System.out, null);
+//					} catch (IOException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+					
+					assertThat(javaInstance.getParticipants()).hasSize(3);
+					assertThat(javaInstance.getResources()).hasSize(5);
+					assertThat(javaInstance.getArtifacts()).hasSize(3);
+					assertThat(javaInstance.getActivities()).hasSize(1);
+
+					// Cross-referencing.
+					Flow flow = (Flow) javaInstance.getActivities().get("development");
+					Activity<?> createActivity = (Activity<?>) flow.getElements().get("create");
+					
+					assertThat(createActivity.getParticipants()).singleElement();
+					assertThat(createActivity.getParticipants().get(0).getName()).isEqualTo("Product Owner");
+					
+					assertThat(createActivity.getResources()).singleElement();
+					assertThat(createActivity.getResources().get(0).getName()).isEqualTo("GitHub Issues");
+					
+					assertThat(javaInstance.getResources().get("ide").getName()).isEqualTo("Eclipse IDE");
+					
+					Activity<?> groomActivity = (Activity<?>) flow.getElements().get("groom");					
+					Transition developOutput = groomActivity.getOutputs().get("develop");
+					assertThat(developOutput.getPayload().get(0)).isEqualTo(javaInstance.getArtifacts().get("story"));
+					
+					Activity<?> developActivity = (Activity<?>) flow.getElements().get("develop");
+					assertThat(developOutput.getTarget()).isEqualTo(developActivity);
+					
+					assertThat(developActivity.getInputArtifacts()).singleElement();
+					assertThat(developActivity.getInputArtifacts().get(0).getName()).isEqualTo("User story");
+					assertThat(developActivity.getInputs()).singleElement();
+					assertThat(developActivity.getResources()).hasSize(3);
+					
+					assertThat(developActivity.getOutputArtifacts()).singleElement();
+					assertThat(developActivity.getOutputArtifacts().get(0).getName()).isEqualTo("Source code");
+										
+				},
+				diagnostic -> {
+					if (diagnostic.getStatus() == Status.FAIL || diagnostic.getStatus() == Status.ERROR) {
+						System.err.println("***********************");
+						System.err.println("*      Diagnostic     *");
+						System.err.println("***********************");
+						diagnostic.dump(System.err, 4, Status.FAIL, Status.ERROR);
+					}
+					assertThat(diagnostic.getStatus()).isEqualTo(Status.SUCCESS);
+				});		
+	}
+	
+	
+	@Test
+	public void testJavaKubernetes() throws Exception {	
+		load(
+				"agile/java-kubernetes.yml", 
+				obj -> {
+					org.nasdanika.flow.Package javaKubernetes = (org.nasdanika.flow.Package) obj;					
+					Package javaKubernetesInstance = javaKubernetes.create();
+					ResourceSet resourceSet = new NcoreResourceSet();
+					resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(org.eclipse.emf.ecore.resource.Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
+					org.eclipse.emf.ecore.resource.Resource resource = resourceSet.createResource(URI.createURI("mem://xxx/java-instance.xml"));
+					resource.getContents().add(javaKubernetesInstance);
+//					try {
+//						resource.save(System.out, null);
+//					} catch (IOException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+					
+					assertThat(javaKubernetesInstance.getParticipants()).hasSize(3);
+					assertThat(javaKubernetesInstance.getResources()).hasSize(6);
+					assertThat(javaKubernetesInstance.getArtifacts()).hasSize(3);
+					assertThat(javaKubernetesInstance.getActivities()).hasSize(1);
+
+					// Cross-referencing.
+					Flow flow = (Flow) javaKubernetesInstance.getActivities().get("development");
+					Activity<?> createActivity = (Activity<?>) flow.getElements().get("create");
+					
+					assertThat(createActivity.getParticipants()).singleElement();
+					assertThat(createActivity.getParticipants().get(0).getName()).isEqualTo("Product Owner");
+					
+					assertThat(createActivity.getResources()).singleElement();
+					assertThat(createActivity.getResources().get(0).getName()).isEqualTo("GitHub Issues");
+					
+					assertThat(javaKubernetesInstance.getResources().get("runtime").getName()).isEqualTo("Kubernetes");
+					
+					Activity<?> groomActivity = (Activity<?>) flow.getElements().get("groom");					
+					Transition developOutput = groomActivity.getOutputs().get("develop");
+					assertThat(developOutput.getPayload().get(0)).isEqualTo(javaKubernetesInstance.getArtifacts().get("story"));
+					
+					Activity<?> developActivity = (Activity<?>) flow.getElements().get("develop");
+					assertThat(developOutput.getTarget()).isEqualTo(developActivity);
+					
+					assertThat(developActivity.getInputArtifacts()).singleElement();
+					assertThat(developActivity.getInputArtifacts().get(0).getName()).isEqualTo("User story");
+					assertThat(developActivity.getInputs()).singleElement();
+					assertThat(developActivity.getResources()).hasSize(4);
+					
+					assertThat(developActivity.getOutputArtifacts()).singleElement();
+					assertThat(developActivity.getOutputArtifacts().get(0).getName()).isEqualTo("Source code");
+										
+				},
+				diagnostic -> {
+					if (diagnostic.getStatus() == Status.FAIL || diagnostic.getStatus() == Status.ERROR) {
+						System.err.println("***********************");
+						System.err.println("*      Diagnostic     *");
+						System.err.println("***********************");
+						diagnostic.dump(System.err, 4, Status.FAIL, Status.ERROR);
+					}
+					assertThat(diagnostic.getStatus()).isEqualTo(Status.SUCCESS);
+				});		
+	}
+	
+	
 }
