293cf18faa409aa2698612864cf3a173d491ea03
Pavel <Pavel.Vlasov@nasdanika.org>
2021-10-18 19:56:44 -0400

    Test fix.

--- flow/src/test/java/org/nasdanika/flow/tests/TestAgile.java
+++ flow/src/test/java/org/nasdanika/flow/tests/TestAgile.java
@@ -130,7 +130,7 @@ public class TestAgile extends TestBase {
 		assertThat(sourceArtifact.getOutputFor()).singleElement();
 		
 		assertThat(core.getParticipants().get("product-owner").getParticipates()).hasSize(2);
-		assertThat(core.getResources().get("issue-tracker").getUsedBy()).hasSize(4);
+		assertThat(core.getResources().get("issue-tracker").getUsedIn()).hasSize(4);
 		
 		assertThat(core.getArtifacts().get("story").getPayloadFor()).hasSize(2);
 		
