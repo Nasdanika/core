9aac1cc937a17c638e997dc24ee2094bab7431cc
Pavel <Pavel.Vlasov@nasdanika.org>
2021-12-04 11:27:59 -0500

    Imports cleanup

--- emf/src/main/java/org/nasdanika/emf/persistence/EObjectSupplierFactory.java
+++ emf/src/main/java/org/nasdanika/emf/persistence/EObjectSupplierFactory.java
@@ -3,7 +3,6 @@ package org.nasdanika.emf.persistence;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
 
