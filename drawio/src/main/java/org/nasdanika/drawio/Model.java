d9b5a200ccab63dada6dec113b2b8167424b6fa9
Pavel <Pavel.Vlasov@nasdanika.org>
2022-12-29 16:54:31 -0500

    Cleanup

--- drawio/src/main/java/org/nasdanika/drawio/Model.java
+++ drawio/src/main/java/org/nasdanika/drawio/Model.java
@@ -1,8 +1,5 @@
 package org.nasdanika.drawio;
 
-import java.util.Map;
-import java.util.function.BiFunction;
-
 /**
  * Represents mxGraphModel element
  * @author Pavel
@@ -14,14 +11,6 @@ public interface Model extends Element {
 	 * @return Diagram root which contains layers.
 	 */
 	Root getRoot();
-		
-	/**
-	 * @param step
-	 * @return A visitor which sizes and arranges nodes in such a way that they do not overlap.
-	 */
-	static BiFunction<Element, Map<Element, Rectangle>, Rectangle> createNonOverlappingLayoutVisitor(int step) {
-		throw new UnsupportedOperationException("TODO - move logic from diagram.gen");
-	}	
 	
 	/**
 	 * Containing page
