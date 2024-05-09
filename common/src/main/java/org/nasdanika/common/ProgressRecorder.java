2969e911915e620906601ad7184a1283dab501cb
Pavel <Pavel.Vlasov@nasdanika.org>
2024-05-09 08:14:31 -0400

    Imports cleanup

--- common/src/main/java/org/nasdanika/common/ProgressRecorder.java
+++ common/src/main/java/org/nasdanika/common/ProgressRecorder.java
@@ -6,7 +6,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CancellationException;
-import java.util.stream.Collectors;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
