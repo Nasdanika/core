24edaae377cb02fee8fb7ac5ebeae641869fcd2b
Pavel <Pavel.Vlasov@nasdanika.org>
2024-05-21 20:31:44 -0400

    Resource factory options

--- cli/src/main/java/org/nasdanika/cli/ResourceSetMixIn.java
+++ cli/src/main/java/org/nasdanika/cli/ResourceSetMixIn.java
@@ -1,5 +1,9 @@
 package org.nasdanika.cli;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.nasdanika.capability.CapabilityLoader;
@@ -8,7 +12,10 @@ import org.nasdanika.capability.ServiceCapabilityFactory;
 import org.nasdanika.capability.ServiceCapabilityFactory.Requirement;
 import org.nasdanika.capability.emf.ResourceSetContributor;
 import org.nasdanika.capability.emf.ResourceSetRequirement;
+import org.nasdanika.common.NasdanikaException;
 import org.nasdanika.common.ProgressMonitor;
+
+import picocli.CommandLine.Option;
 
 /**
  * Mixes in a {@link ResourceSet} loaded by {@link CapabilityLoader}.
@@ -16,16 +23,21 @@ import org.nasdanika.common.ProgressMonitor;
  *
  */
 public class ResourceSetMixIn {
-
-	// TODO - includes/excludes for epackages, resource and adapter factories. Resource factories - maps - extensions, protocols, content types 
-//	@Option(names = {"-p", "--progress"}, description = "Output file for progress monitor")
-//	private File progressOutput;
-//	
-//	@Option(names = {"-j", "--json"}, description = "Output progress in JSON")
-//	private boolean jsonProgress;
-//	
-//	@Option(names = {"-d", "--data"}, description = "Output progress data")
-//	private boolean data;
+	
+	@Option(
+			names = "--extension-resource-factory",
+			description = "Maps extension to resource factory class")
+    Map<String, Class<?>> extensionResourceFactories;	
+	
+	@Option(
+			names = "--protocol-resource-factory",
+			description = "Maps protocol to resource factory class")
+    Map<String, Class<?>> protocolResourceFactories;
+	
+	@Option(
+			names = "--content-type-resource-factory",
+			description = "Maps content type to resource factory class")
+    Map<String, Class<?>> contentTypeResourceFactories;
 		
 	public ResourceSet createResourceSet(ProgressMonitor progressMonitor) {
 		CapabilityLoader capabilityLoader = getCapabilityLoader();
@@ -39,7 +51,36 @@ public class ResourceSetMixIn {
 	}
 	
 	protected ResourceSet configureResourceSet(ResourceSet resourceSet) {
-		// TODO - explicit registrations
+		if (extensionResourceFactories != null) {
+			Map<String, Object> extensionToFactoryMap = resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap();
+			for (Entry<String, Class<?>> ee: extensionResourceFactories.entrySet()) {
+				try {
+					extensionToFactoryMap.put(ee.getKey(), ee.getValue().getDeclaredConstructor().newInstance());
+				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException	| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+					throw new NasdanikaException("Could not instantiate resource factory " + ee.getValue() + ": " + e, e);
+				}
+			}
+		}
+		if (protocolResourceFactories != null) {
+			Map<String, Object> protocolToFactoryMap = resourceSet.getResourceFactoryRegistry().getProtocolToFactoryMap();
+			for (Entry<String, Class<?>> pe: protocolResourceFactories.entrySet()) {
+				try {
+					protocolToFactoryMap.put(pe.getKey(), pe.getValue().getDeclaredConstructor().newInstance());
+				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException	| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+					throw new NasdanikaException("Could not instantiate resource factory " + pe.getValue() + ": " + e, e);
+				}
+			}
+		}
+		if (contentTypeResourceFactories != null) {
+			Map<String, Object> contentTypeToFactoryMap = resourceSet.getResourceFactoryRegistry().getContentTypeToFactoryMap();
+			for (Entry<String, Class<?>> ce: contentTypeResourceFactories.entrySet()) {
+				try {
+					contentTypeToFactoryMap.put(ce.getKey(), ce.getValue().getDeclaredConstructor().newInstance());
+				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException	| InvocationTargetException | NoSuchMethodException | SecurityException e) {
+					throw new NasdanikaException("Could not instantiate resource factory " + ce.getValue() + ": " + e, e);
+				}
+			}
+		}
 		return resourceSet;
 	}
 
@@ -60,7 +101,7 @@ public class ResourceSetMixIn {
 	}
 		
 	protected boolean testContributor(ResourceSetContributor contributor) {
-		return true; // TODO - include/exclude epackages, resource and adapter factories
+		return true; 
 	}
 	
 	protected boolean testFactory(ServiceCapabilityFactory<ResourceSetRequirement, ResourceSet> factory) {
