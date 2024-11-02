4c3633f897fc837d757722f86ef9b1a9695d9578
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2024-11-02 04:35:27 -0400

    NPE fix

--- graph/src/main/java/org/nasdanika/graph/processor/ReflectiveRegistryWirer.java
+++ graph/src/main/java/org/nasdanika/graph/processor/ReflectiveRegistryWirer.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
@@ -47,7 +48,7 @@ public class ReflectiveRegistryWirer<P> extends Reflector {
 		processorAnnotatedElementRecords
 			.filter(ae -> ae.getAnnotation(RegistryEntry.class) != null)
 			.filter(ae -> ae.mustSet(null, "Fields/methods annotated with RegistryEntry must have (parameter) type assignable from the processor type or ProcessorInfo if info is set to true: " + ae.getAnnotatedElement()))
-			.flatMap(setterRecord -> registry.stream().map(re -> new RegistryMatch(setterRecord.getAnnotation(RegistryEntry.class), setterRecord, re)))
+			.flatMap(setterRecord -> registry.stream().filter(Objects::nonNull).map(re -> new RegistryMatch(setterRecord.getAnnotation(RegistryEntry.class), setterRecord, re)))
 			.filter(rm -> evaluatePredicate(rm.config.getElement(), rm.annotation.value(), variables))
 			.forEach(rm -> {
 				infoProvider.accept(rm.config.getElement(), (rpInfo, pMonitor) -> {
