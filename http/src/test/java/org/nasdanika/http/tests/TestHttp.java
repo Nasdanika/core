90351db0498cdf08bdf8a8f6fd127e238cd4475c
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2025-04-08 18:22:46 -0400

    HTTP tests

--- http/src/test/java/org/nasdanika/http/tests/TestHttp.java
+++ http/src/test/java/org/nasdanika/http/tests/TestHttp.java
@@ -10,6 +10,7 @@ import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
@@ -74,7 +75,45 @@ public class TestHttp {
 		} finally {
 			span.end();
 		}
+	}
+	
+	@Test
+	@Disabled
+	public void testAsyncClientWithTelemetry() {
+		OpenTelemetry openTelemetry = GlobalOpenTelemetry.get();
+		Tracer tracer = openTelemetry.getTracer(TestHttp.class.getName() + ".testAsyncClientWithTelemetry");
+		Span span = TelemetryUtil.buildSpan(tracer.spanBuilder("Hello"))
+			.setSpanKind(SpanKind.CLIENT)
+			.startSpan();
 		
+		TextMapPropagator propagator = openTelemetry.getPropagators().getTextMapPropagator();
+
+		try (Scope scope = span.makeCurrent()) {			
+			HttpClient client = HttpClient.create()
+					.headers(headerBuilder -> {
+						propagator.inject(Context.current(), headerBuilder, (c, k, v) -> c.set(k,v));	
+					})
+					.followRedirect(true);
+			
+			String response = client.get()
+				.uri("http://localhost:8080/privet")			
+				.responseContent()
+				.aggregate()
+				.asString()
+				.map(result -> {
+					span.setStatus(StatusCode.OK);
+					return result;
+				})
+				.onErrorMap(error -> {
+					span.recordException(error);
+					span.setStatus(StatusCode.ERROR);
+					return error;
+				})
+				.doFinally(signal -> span.end())
+				.block();
+			
+			System.out.println(response);
+		}
 	}
 
 }
