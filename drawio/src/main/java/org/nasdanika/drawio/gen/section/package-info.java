e0245aeaaaf456fa0e7ffb6aefd0a5d1992e94d9
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2025-09-15 18:48:09 -0400

    Deleted handler experiments and drawio sections to reintroduce later

--- drawio/src/main/java/org/nasdanika/drawio/gen/section/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Classes for generating {@link org.nasdanika.common.Section}s and {@link org.nasdanika.common.Content} from
- * diagram elements.
- */
-package org.nasdanika.drawio.gen.section;
